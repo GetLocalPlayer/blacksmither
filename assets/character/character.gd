59b89c22ea27095b9a25731f5264b6aae3df1314
GetLocalPlayer <GetLocalPlayer@gmail.com>
2025-03-01 18:28:28 +0400

    Some visuals for easier testing

---
+++ assets/character/character.gd
@@ -0,0 +1,7 @@
+extends CharacterBody3D
+
+
+## Movement speed
+@export var speed: float = 2
+## Turning speed in radians
+@export var turn_speed: float = 2
--- characters/test_character/test_character.gd
+++
@@ -1,4 +0,0 @@
-extends CharacterBody3D
-
-
-@export var speed: float = 2
