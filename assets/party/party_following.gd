7c52eaf6e778004715248b8d6b6ffdee613bed7e
GetLocalPlayer <GetLocalPlayer@gmail.com>
2025-03-03 15:59:38 +0400

    Party - snake reversed

--- assets/party/party_following.gd
+++ assets/party/party_following.gd
@@ -6,9 +6,9 @@ extends Path3D
 # implement snake-like party movement.
 
 ## The distance at which following starts
-@export var _start_distance: float = 2
+@export var _start_distance: float = 2.
 ## The distance at which following stops
-@export var _stop_distance: float = 0.5
+@export var _stop_distance: float = 1.5
 
 @onready var _body: CharacterBody3D = owner
 @onready var _move_speed: float = (owner as Party).speed
@@ -20,43 +20,37 @@ func _ready() -> void:
 	curve = Curve3D.new()
 	curve.bake_interval = 999999 # it must be the max of int, but there's no such constant in godot api yet
 	curve.add_point(_body.global_position)
-	curve.add_point(_body.global_position)
+	curve.add_point(_body.global_position + (_body.global_basis.z * (_start_distance * get_child_count())))
 	for child: PathFollow3D in get_children():
-		child.progress = 0.
-		child.use_model_front = true
+		child.progress_ratio = 0.
+		child.use_model_front = false
 
 
 func _physics_process(_delta: float) -> void:
 	var new_pos: Vector3 = _body.global_position
-	# The end of the curve has to be the path's head
-	# otherwise we have to calculate all the offsets
-	# of the FollowPath3D's due happening offset
-	# when the points added into the beginning.
-	var tail: Vector3 = curve.get_point_position(curve.point_count - 1)
-	if is_zero_approx(tail.distance_squared_to(new_pos)):
-		return
-	var tail_tail: Vector3 = curve.get_point_position(curve.point_count - 2)
-	if is_equal_approx((new_pos - tail).dot(tail - tail_tail), 1.):
-		curve.set_point_position(curve.point_count - 1, new_pos)
-	else:
-		print(new_pos.distance_to(tail))
-		curve.add_point(new_pos)
+	var head: Vector3 = curve.get_point_position(0)
+	var offset: float = 0.
+	if not is_zero_approx(head.distance_squared_to(new_pos)):
+		var after_head: Vector3 = curve.get_point_position(1)
+		if is_equal_approx((new_pos - head).dot(head - after_head), 1.):
+			curve.set_point_position(0, new_pos)
+		else:
+			curve.add_point(new_pos, Vector3.ZERO, Vector3.ZERO, 0)
+		offset = new_pos.distance_to(head)
+
 	if get_child_count() > 0:
-		(get_child(0) as PathFollow3D).progress_ratio = 1.
-		var length: float = curve.get_baked_length()
+		(get_child(0) as PathFollow3D).progress_ratio = 0.
 		for i: int in range(1, get_child_count()):
 			var slot: PathFollow3D = get_child(i)
+			slot.progress += offset
+			if slot.name == "Slot2": print(slot.progress)
 			if not _moving_slots.has(slot):
-				if length - slot.progress > _start_distance * i :
+				if slot.progress > _start_distance * i :
 					_moving_slots.append(slot)
 			if _moving_slots.has(slot):
-				slot.progress += _delta * _move_speed
-				if slot.name == "Slot2":
-					print(slot.name, ' ', length - slot.progress, ' ', _stop_distance * i)
-					print(slot.progress)
-					print()
-				# if length - slot.progress < _stop_distance * i:
-				# 	slot.progress = _stop_distance * i
-				# 	_moving_slots.erase(slot)
+				slot.progress -= _delta * _move_speed
+				if slot.progress <= _stop_distance * i:
+					slot.progress = _stop_distance * i
+					_moving_slots.erase(slot)
 			
 	
