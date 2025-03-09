2d8789c998d1b58e647a1c930ecb5f8a2aa77d80
GetLocalPlayer <GetLocalPlayer@gmail.com>
2025-03-09 09:26:59 +0400

    Remove spell gd, stached changes

--- assets/combat_scene/character.gd
+++ assets/combat_scene/character.gd
@@ -2,42 +2,6 @@ extends Node3D
 class_name Character
 
 
-<<<<<<< Updated upstream
-@export var health: float = 1000:
-	get:
-		return health
-	set(value):
-		health = value if value >= 0. else 0.
-		if is_node_ready():
-			if health == 0.:
-				die()
-
-
-var is_alive: bool:
-	get:
-		return health > 0
-
-
-@onready var _playback: AnimationNodeStateMachinePlayback = $Model/AnimationTree.get("parameters/playback")
-
-var _anims: Dictionary = {
-	impact = "damaged_impact",
-	death = "death",
-}
-
-
-func take_damage(value: float, play_impact_anim: bool = true) -> void:
-	health -= value
-	if play_impact_anim:
-		_playback.travel(_anims.impact)
-
-
-func die() -> void:
-	if health > 0:
-		health = 0
-	else:
-		_playback.travel(_anims.death)
-=======
 @onready var _clicker: StaticBody3D = $Clicker
 @onready var _target: Panel = $HealthBar/Target
 
@@ -57,4 +21,3 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
     _target.hide()
->>>>>>> Stashed changes
