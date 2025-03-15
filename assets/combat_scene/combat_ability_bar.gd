25fed10dbf4050b51efea7234ccdc5bcba1b58ad
GetLocalPlayer <GetLocalPlayer@gmail.com>
2025-03-15 07:31:42 +0400

    Simplifying

--- assets/combat_scene/combat_ability_bar.gd
+++ assets/combat_scene/combat_ability_bar.gd
@@ -2,37 +2,14 @@ extends HBoxContainer
 class_name CombatAbilityBar
 
 
-signal ability_selected()
-
-
-# This node is the basic setup to create buttons
 # for abilities duplicating it.
 @onready var _base_button: TextureButton = $BaseButton
-@onready var _selector: Panel = $BaseButton/AbilitySelector
-var _selected_ability: CombatAbility = null:
-	get:
-		return _selected_ability
-	set(value):
-		if is_node_ready() and not value and _selector.get_parent():
-			_selector.hide()
-			_selector.get_parent().remove_child(_selector)
-		_selected_ability = value
-
-
-func clear_selection() -> void:
-	_selected_ability = null
-
-
-func get_selected_ability() -> CombatAbility:
-	return _selected_ability
+@onready var button_group = _base_button.button_group
 
 
 func _ready() -> void:
 	remove_child(_base_button)
 	_base_button.hide()
-	_base_button.remove_child(_selector)
-	_selector.hide()
-
 
 # Recycling of button abilities not to create new
 # ones for each unique ability. Probably an overkill
@@ -47,18 +24,9 @@ func _recycle_button(button: TextureButton) -> void:
 	button.hide()
 	if button.parent:
 		button.parent.remove_child(button)
-	if _selector.get_parent():
-		_selector.get_parent().remove_child(_selector)
-		_selector.hide()
 	for signal_dict: Dictionary in button.get_signal_list():
 		for connection: Dictionary in button.get_signal_connection_list(signal_dict.name):
 			connection.signal.disconnect(connection.callable)
-
-
-func _get_button() -> TextureButton:
-	if not _recycled_buttons.is_empty():
-		return _recycled_buttons.pop_back()
-	return _base_button.duplicate()
 
 
 func set_abilities(abilities: Array[CombatAbility]) -> void:
@@ -78,34 +46,3 @@ func set_abilities(abilities: Array[CombatAbility]) -> void:
 		btn.texture_normal = abi.icon
 		btn.texture_pressed = abi.icon
 		btn.show()
-		btn.pressed.connect(_on_button_pressed.bind(btn, abi))
-
-
-func _on_button_pressed(button: TextureButton, ability: CombatAbility) -> void:
-	if _selector.get_parent():
-		_selector.get_parent().remove_child(_selector)
-		_selector.hide()
-	if ability.cast_type == CombatAbility.CastType.TARGET:
-		button.add_child(_selector)
-		_selector.show()
-	elif _selector.get_parent():
-		_selector.get_parent().remove_child(_selector)
-		_selector.hide()
-	_selected_ability = ability
-	ability_selected.emit()
-
-
-# Hide SpellSelection node on any click.
-# If the click was on a target - the spell
-# is used and the SpelLSeleciton isn't needed
-# anymore.
-# If the click was on an empty space, spell
-# selection must be cleared.
-# If the click was on the same spell, it
-# will just make SpellSeleciton back visible
-# right away.
-# func _input(event: InputEvent) -> void:
-# 	var mouse_click: InputEventMouseButton = event as InputEventMouseButton
-# 	if mouse_click and mouse_click.pressed and (mouse_click.button_index == MOUSE_BUTTON_LEFT or mouse_click.button_index == MOUSE_BUTTON_MASK_RIGHT):
-# 		if _selector.visible:
-# 			_selector.hide()
--- assets/combat_scene/combat_scene.gd
+++ assets/combat_scene/combat_scene.gd
@@ -1,35 +1,42 @@
 extends Node3D
 
 
-var _slot_action_queue: Array[CombatSlot] = []
+const BOT_GROUP = "bot"
+const PLAYER_GROUP = "player"
+const ENEMY_GROUP = "enemies"
+@onready var _characters: Array[CombatCharacter] = Array($Characters.get_children(), TYPE_OBJECT, "Area3D", CombatCharacter)
+@onready var _ability_bar: CombatAbilityBar = $UIBase/AbilityBar
+@onready var _health_bar_base: ProgressBar = $UIBase/UIBase/HealthBar
+@onready var _clicker: Area3D = $Clicker
 
-const ALLY_GROUP = "player_slots"
-const ENEMY_GROUP = "enemy_slots"
-@onready var _slots: CombatSlotGroup = $Slots
-@onready var _ability_bar: CombatAbilityBar = $AbilityBar
+
+var _character_queue: Array[CombatCharacter] = []
+
+
+func _init_for_testing() -> void:
+	_character_queue = _characters.duplicate() 
+
 
 func _ready() -> void:
+	_init_for_testing()
+	_clicker.input_event.connect(_on_clicker_input_event)
 	# Setting action queue for slots
-	for slot: CombatSlot in _slots.get_children():
-		var character: CombatCharacter = slot.get_character()
-		if character:
-			_slot_action_queue.append(slot)
-			slot.mouse_entered.connect(_on_mouse_entered_slot_character)
-			slot.mouse_exited.connect(_on_mouse_exited_slot_character)
-			slot.mouse_pressed.connect(_on_slot_character_mouse_pressed)
+	
+	for c in _characters:
+		c.mouse_entered.connect(_on_mouse_entered_slot_character)
+		c.mouse_exited.connect(_on_mouse_exited_slot_character)
+		c.mouse_pressed.connect(_on_slot_character_mouse_pressed)
 	_ability_bar.ability_selected.connect(_on_ability_pressed)
 	_run_next_turn()
 
 
 func _run_next_turn() -> void:
 	_ability_bar.clear_selection()
-	var slot: CombatSlot = _slot_action_queue.pop_front() as CombatSlot
-	slot.selected = true
-	# Check if the current slot belongs
-	# to the player or to the bot
-	if _slots.slot_is_player(slot):
-		_ability_bar.set_abilities(slot.get_character().get_abilities())
-	if _slots.slot_is_bot(slot):
+	var c: CombatCharacter = _character_queue.pop_front()
+	if c.is_in_group(PLAYER_GROUP):
+		_ability_bar.set_abilities(c.get_abilities())
+	if c.is_in_group(BOT_GROUP):
+		#start bot actions
 		pass
 
 
@@ -60,4 +67,14 @@ func _on_mouse_exited_slot_character(slot: CombatSlot, _character: CombatCharact
 
 
 func _on_slot_character_mouse_pressed(slot: CombatSlot, character: CombatCharacter) -> void:
+	if not slot.target_marks.primary.visible:
+		return
 	prints("Cast spell", _ability_bar.get_selected_ability(), "on", character, "in", slot)
+	_ability_bar.clear_selection()
+	slot.target_marks.primary.hide()
+
+
+func _on_clicker_input_event(_c: Node, e: InputEvent, _ep: Vector3, _n: Vector3, _si: int) -> void:
+	var me: InputEventMouseButton = e as InputEventMouseButton
+	if me and me.pressed and (me.button_index == MOUSE_BUTTON_LEFT or me.button_index == MOUSE_BUTTON_MASK_RIGHT):
+		_ability_bar.clear_selection()
